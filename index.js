e439099467089f97f72889b98b81f2dc1909c43d
wenyu0416 <wenyu0416@163.com>
2021-10-07 17:54:02 +0800

    1st commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const hostname = "localhost";
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+})
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('not support update all dishes in one go!');
+})
+
+app.post('/dishes', (req, res, next) => {
+    res.end('will add the dish: ' + req.body.name + ' with details ' + req.body.description);
+})
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('will delete all the dishes for you!');
+})
+
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send the details of dish ' + req.params.dishId + ' to you!');
+})
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.end('will update the dish: ' + req.body.name + ' with details ' + req.body.description);
+})
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('not support add single dishe!');
+})
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+})
+
+app.use((req, res, next) => {
+    console.log("Request for " + req.url + " by method " + req.method);
+
+    if (req.method == 'GET') {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.url + ' not found</h1></body></html>');
+        return;
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.method + ' not support</h1></body></html>');
+        return;
+    }
+})
+
+const server_express = http.createServer(app);
+
+server_express.listen(port, hostname, () => {
+    console.log(`sever running at http://${hostname}:${port}`)
+})
